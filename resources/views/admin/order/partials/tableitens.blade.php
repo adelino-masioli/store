a6dfa85cc57669185c99c7068592c35631133054
alfjuniorbh <alfjuniorbh.web@gmail.com>
2018-10-04 22:16:57 -0300

    Adj code items order

--- resources/views/admin/order/partials/tableitens.blade.php
+++ resources/views/admin/order/partials/tableitens.blade.php
@@ -1,7 +1,7 @@
 <table class="table table-responsive table-striped table-condensed table-hover table-bordered" style="width: 100%;">
     <thead>
     <tr>
-        <th class="col-md-1 text-center">ID</th>
+        <th class="col-md-1 text-center">CÓDIGO</th>
         <th class="col-md-1 text-center">AÇÃO</th>
         <th class="col-md-7 text-center">NOME DO PRODUTO</th>
         <th class="col-md-1 text-center">PREÇO</th>
@@ -12,7 +12,7 @@
     <tbody>
     @foreach($items as $item)
         <tr>
-            <td class="col-md-1 text-center">{{$item->id}}</td>
+            <td class="col-md-1 text-center">{{$item->product_id}}</td>
             <td class="col-md-1 text-center"><button class="btn btn-xs bg-red" onclick="destroyItem('{{$item->id}}');"><i class="fa fa-trash"></i></button></td>
             <td class="col-md-7 text-left">{{$item->product_name}}</td>
             <td class="col-md-1 text-right">{{money_br($item->price)}}</td>
